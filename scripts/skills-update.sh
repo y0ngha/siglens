7ad7006988a574ddbd280ddaf95530d1b6af35e9
y0ngha <dev.y0ngha@gmail.com>
2026-04-02 17:17:08 +0900

    chore: skills-update 스크립트 작성

---
+++ scripts/skills-update.sh
@@ -0,0 +1,6 @@
+npx skills add vercel-labs/agent-skills@vercel-react-best-practices
+npx skills add https://github.com/vercel-labs/next-skills --skill next-best-practices
+npx skills add https://github.com/vercel-labs/next-skills --skill next-cache-components
+npx skills add https://github.com/wshobson/agents --skill typescript-advanced-types
+npx skills add https://github.com/vercel-labs/agent-skills --skill web-design-guidelines
+npx skills add https://github.com/anthropics/skills --skill frontend-design
