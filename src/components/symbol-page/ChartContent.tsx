7b73f4ed2919c5c3b2121778cbf6ec79ec688603
y0ngha <dev.y0ngha@gmail.com>
2026-04-05 02:54:57 +0900

    refactor: import 순서 변경

--- src/components/symbol-page/ChartContent.tsx
+++ src/components/symbol-page/ChartContent.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useMemo, useState } from 'react';
 import type React from 'react';
+import { useMemo, useState } from 'react';
 import type {
     AnalysisResponse,
     Bar,
